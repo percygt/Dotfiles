ecb336ef30fd80ac57984b9b2f3601d01e08c2ef
Percy Timon <percygt.dev@gmail.com>
2023-10-09 06:01:35 +0800

    Revert "auto-commit 💾"
    This reverts commit 06ce00418764544d37147ac9df3abde924c4f3d4.

--- stow/fish/.config/fish/functions/funcadd.fish
+++ stow/fish/.config/fish/functions/funcadd.fish
@@ -1,22 +1,4 @@
 function funcadd --description "Add and save aliases"
-	set cf (status function)
-        set -l options 'h/help'
-        argparse -n funcadd $options -- $argv
-        or return
-
-        # should create a manpage
-        if set -q _flag_help
-            __fish_print_help cf
-            return 0
-        end
-
-	if not set -q argv[1]
-            printf (_ "%ls: Expected at least %d args, got only %d\n") $cf 1 0
-            return 1
-        end
-
-	set -l retval 0
-
 	alias $argv
 	funcsave $argv[1]
 	echo "alias $argv[1]='$argv[2]'" >> $HOME/.bashrc_aliases
