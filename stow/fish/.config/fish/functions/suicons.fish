2700d5167ac7637abef904f527376696a88aef91
Percy Timon <percygt.dev@gmail.com>
2023-10-13 15:33:22 +0800

    auto-commit 💾

---
+++ stow/fish/.config/fish/functions/suicons.fish
@@ -0,0 +1,4 @@
+function suicons --wraps='sudo cp -r .icons/. /usr/share/icons/' --description 'alias suicons sudo cp -r .icons/. /usr/share/icons/'
+  sudo cp -r .icons/. /usr/share/icons/ $argv
+        
+end
---
+++ stow/fish/.config/fish/functions/suthemes.fish
@@ -0,0 +1,4 @@
+function suthemes --wraps='sudo cp -r .themes/. /usr/share/themes/' --description 'alias suthemes sudo cp -r .themes/. /usr/share/themes/'
+  sudo cp -r .themes/. /usr/share/themes/ $argv
+        
+end
