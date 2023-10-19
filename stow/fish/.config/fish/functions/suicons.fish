0ae6571bc471388b71634273674edac89b6aabae
Percy Timon <percygt.dev@gmail.com>
2023-10-19 13:35:27 +0800

    auto-commit 💾

--- stow/fish/.config/fish/functions/suicons.fish
+++ stow/fish/.config/fish/functions/suicons.fish
@@ -1,4 +1,4 @@
-function suicons --wraps='sudo cp -r .icons/. /usr/share/icons/' --description 'alias suicons sudo cp -r .icons/. /usr/share/icons/'
-  sudo cp -r .icons/. /usr/share/icons/ $argv
+function suicons --wraps='sudo cp -r .icons/. /usr/share/icons/' --wraps='sudo cp -r ~/.local/share/icons/. /usr/share/icons/' --description 'alias suicons sudo cp -r ~/.local/share/icons/. /usr/share/icons/'
+  sudo cp -r ~/.local/share/icons/. /usr/share/icons/ $argv
         
 end
