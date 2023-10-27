28c4a63aaad6d7b120453b2981e38f66588e97c3
Percy Timon <percygt.dev@gmail.com>
2023-10-27 16:20:47 +0800

    auto-commit 💾

--- stow/fish/.config/fish/config.fish
+++ stow/fish/.config/fish/config.fish
@@ -1,11 +1,7 @@
 if status is-interactive
 	starship init fish | source
+	fastfetch
  # Commands to run in interactive sessions can go here
-end
-
-set -l count (ps a | awk '{print $2}' | grep -vi "tty*" | uniq | wc -l);
-if test $count -le 3
-     neofetch
 end
 
 pyenv init - | source
