2fe4752d756b545af376dc138462585eb13b6c68
Percy Timon <percygt.dev@gmail.com>
2023-11-01 03:03:00 +0800

    .

--- setup.sh
+++ setup.sh
@@ -3,7 +3,8 @@ set -eu
 
 [ "$UID" -eq 0 ] || { echo "This script must be run as root."; exit 1;} # Need to figure out how to pkexec so we only ask for the password once.
 
-stow -d ./stow -t $HOME .
+
+
 
 SC=./scripts
 source $SC/common
