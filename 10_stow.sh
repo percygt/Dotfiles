78544ce6290122aa884decc65b960756ebd9415a
Percy Timon <percygt.dev@gmail.com>
2023-11-03 05:52:43 +0800

    auto-commit 💾

--- 10_stow.sh
+++ 10_stow.sh
@@ -4,7 +4,7 @@ set -eu
 [ "$UID" -ne 0 ] || { echo "This script must be run by $SUDO_USER."; exit 1;}
 
 # cd stow
-stow -t $HOME -d ~/Dotfiles/stow/ */
+stow -t $HOME -d ~/Dotfiles/stow */
 
 echo -e "\033[0;33mMOVE PERMANENT DATA to /data"
 
