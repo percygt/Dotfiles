45e65b0642144b254c684dd721f3e7deae8abb58
Percy Timon <percygt.dev@gmail.com>
2023-11-04 02:54:26 +0800

    auto-commit 💾

--- 11_homestow.sh
+++ 11_homestow.sh
@@ -8,7 +8,7 @@ set -eu
 stow --adopt -d /data/ -t $HOME shared_home/
 git restore .
 
-
 grep "#aliases" $HOME/.bashrc || echo -e "\n#aliases\n[ -f $HOME/.bash_aliases ] && source $HOME/.bash_aliases" >> $HOME/.bashrc
 grep "#user_configs" $HOME/.bashrc || echo -e "\n#user_configs\n[ -f $HOME/.bash_configs ] && source $HOME/.bash_configs" >> $HOME/.bashrc
 
+sed -i "s/#Experimental = false/Experimental = true/" /etc/bluetooth/main.conf
