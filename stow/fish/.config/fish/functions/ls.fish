2bc820445af4556663e83ac3863665a2fda0b28a
Percy Timon <percygt.dev@gmail.com>
2023-11-03 19:49:39 +0800

    auto-commit 💾

--- stow/fish/.config/fish/functions/ls.fish
+++ stow/fish/.config/fish/functions/ls.fish
@@ -1,4 +1,4 @@
-function ls --wraps='exa -la' --wraps='exa --long --header --git' --wraps='exa --icons -F -H --group-directories-first --git -1' --wraps='exa --icons -FH --time-style=long-iso --group-directories-first -1' --wraps='exa --icons -FH --group-directories-first -1' --wraps='exa --icons -F -H --group-directories-first -1' --description 'alias ls exa --icons -F -H --group-directories-first -1'
-  exa --icons -F -H --group-directories-first -1 $argv
+function ls --wraps='exa -la' --wraps='exa --long --header --git' --wraps='exa --icons -F -H --group-directories-first --git -1' --wraps='exa --icons -FH --time-style=long-iso --group-directories-first -1' --wraps='exa --icons -FH --group-directories-first -1' --wraps='exa --icons -F -H --group-directories-first -1' --wraps='eza --icons -F -H --group-directories-first -1' --description 'alias ls eza --icons -F -H --group-directories-first -1'
+  eza --icons -F -H --group-directories-first -1 $argv
         
 end
