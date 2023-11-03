290c498ac1fdda6445d20fac8b1a7ed04d807316
Percy Timon <percygt.dev@gmail.com>
2023-11-03 10:19:04 +0800

    auto-commit 💾

--- 11_homestow.sh
+++ 11_homestow.sh
@@ -3,6 +3,14 @@ set -eu
 
 [ "$UID" -ne 0 ] || { echo "This script must be run by $SUDO_USER."; exit 1;}
 
-stow -d /data/shared_home/ -t $HOME */
+rm -rf $HOME/Documents
+rm -rf $HOME/Downloads
+rm -rf $HOME/Desktop
+rm -rf $HOME/Music
+rm -rf $HOME/Pictures
+rm -rf $HOME/Videos
+
+
+stow -d /data/ -t $HOME shared_home/
 
 
