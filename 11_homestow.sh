791cf58190bbff7056322f660249636d5e158ae2
Percy Timon <percygt.dev@gmail.com>
2023-11-04 01:44:48 +0800

    auto-commit 💾

--- 11_homestow.sh
+++ 11_homestow.sh
@@ -3,14 +3,10 @@ set -eu
 
 [ "$UID" -ne 0 ] || { echo "This script must be run by $SUDO_USER."; exit 1;}
 
-rm -rf $HOME/Documents
-rm -rf $HOME/Downloads
-rm -rf $HOME/Desktop
-rm -rf $HOME/Music
-rm -rf $HOME/Pictures
-rm -rf $HOME/Videos
 
-stow -d /data/ -t $HOME shared_home/
+
+stow --adopt -d /data/ -t $HOME shared_home/
+git restore .
 
 
 [ grep "#aliases" $HOME/.bashrc ] || echo -e "\n#aliases\n[ -f $HOME/.bash_aliases ] && source $HOME/.bash_aliases" >> $HOME/.bashrc
