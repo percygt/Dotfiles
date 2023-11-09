bb6402f8ad8dd8595d1d9d09dd1aff78e42cb714
Percy Timon <percygt.dev@gmail.com>
2023-11-09 08:34:25 +0800

    auto-commit 💾

---
+++ 10_grubtheme.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -eu
+
+[ "$UID" -eq 0 ] || { echo "This script must be run as root."; exit 1;} # Need to figure out how to pkexec so we only ask for the password once.
+
+wget -P /tmp https://github.com/shvchk/poly-dark/raw/master/install.sh
+
+bash /tmp/install.sh
+
+cp ./files/background.png /boot/grub2/themes/poly-dark/
+
+
+
+cat /etc/default/grub
