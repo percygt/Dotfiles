178f0a610767e913dabf6c91f38bf5dc863fb5b8
Percy Timon <percygt.dev@gmail.com>
2023-11-17 13:57:49 +0800

    💾

--- stow/fish/.config/fish/config.fish
+++ stow/fish/.config/fish/config.fish
@@ -1,7 +1,12 @@
 if status is-interactive
-	starship init fish | source
 	fastfetch
  # Commands to run in interactive sessions can go here
 end
+
+function starship_transient_prompt_func
+  starship module character
+end
+starship init fish | source
+enable_transience
 
 pyenv init - | source
--- stow/fish/.config/fish/functions/logs.fish
+++ stow/fish/.config/fish/functions/logs.fish
@@ -1,3 +1,4 @@
 function logs --wraps='cd ~/Logs && tail -n40' --description 'alias logs cd ~/Logs && tail -n40'
   cd ~/Logs && tail -n40 $argv | bat -l log
+  cd -
 end
