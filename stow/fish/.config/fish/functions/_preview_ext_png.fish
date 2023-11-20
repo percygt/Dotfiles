faf02cde062fd342fed1177bdc6ea7443d54d8b2
Percy Timon <percygt.dev@gmail.com>
2023-11-20 21:25:21 +0800

    💾

--- stow/fish/.config/fish/functions/_preview_ext_png.fish
+++
@@ -1,7 +0,0 @@
-if ! functions --query _preview_viewer_viu
-    exit
-end
-
-function _preview_ext_png
-    _preview_viewer_viu $argv
-end
--- stow/fish/.config/fish/functions/_preview_mime_image.fish
+++ stow/fish/.config/fish/functions/_preview_mime_image.fish
@@ -3,6 +3,6 @@ command --query viu || exit
 set --local cmd (status basename | path change-extension "")
 
 function $cmd
-    #timg -g (math $COLUMNS - 2)x$LINES --frames 1 $argv
-    viu -h 20 $argv
+    timg -t (math $COLUMNS / 2),$LINES $argv
+    #viu -h 20 $argv
 end
