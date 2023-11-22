e4c35b23eae2d1b48580076d49ee6c64aa0759ba
Percy Timon <percygt.dev@gmail.com>
2023-11-22 10:33:28 +0800

    💾

--- 01_initial.sh
+++ 01_initial.sh
@@ -3,6 +3,11 @@ set -eu
 
 [ "$UID" -eq 0 ] || { echo "This script must be run as root."; exit 1;}
 
+sed -i 's/zstd:1/lzo/g' /etc/fstab
+
+COMMON=lsblk -f | awk '/COMMON/{print $4}'
+BACKUP=lsblk -f | awk '/BACKUP/{print $4}'
+WINDOWS=lsblk -f | awk '/WINDOWS/{print $4}'
 
 echo -e "Unsetting grub menu auto hide . . .\n"
 grub2-editenv - unset menu_auto_hide
@@ -14,6 +19,7 @@ btrfs filesystem show
 
 [ -e ./repos ] && cp -Rnvp ./repos/* /etc/yum.repos.d/ &&
 
+
 #Docker
 echo "Settiing Docker repo . . ."
 dnf config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
@@ -24,8 +30,6 @@ dnf config-manager --set-enabled fedora-cisco-openh264
 
 #COPR
 echo "Settiing Copr and rpmfusion repos. . ."
-dnf -y copr enable atim/starship
-dnf -y copr enable atim/lazydocker
 dnf -y copr enable lukenukem/asus-linux
 #RPMfusion
 dnf -y install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
