d9c7fd2877c9f93fa3888ecc80d6ca5118c69d8c
Percy Timon <percygt.dev@gmail.com>
2024-01-17 03:48:36 +0800

    Remove home-manager to be added as a sub-module

--- home-manager/program/direnv.nix
+++
@@ -1,8 +0,0 @@
-{
-  programs.direnv = {
-    enable = true;
-    # enableFishIntegration = true;
-    enableBashIntegration = true;
-    nix-direnv.enable = true;
-  };
-}
--- home-manager/program/qt/default.nix
+++
@@ -1,9 +0,0 @@
-{
-  xdg.configFile = {
-    "Kvantum" = {
-      recursive = false;
-      source = ../../common/qt/Kvantum;
-    };
-    kdeglobals.source = ../../common/qt/kdeglobals;
-  };
-}
